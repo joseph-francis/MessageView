536e70fcb53de9ac0e3069924d031b84f5af66f3
Joseph Francis <josephfrancis100@gmail.com>
2019-07-12 13:12:42 -0500

    Initial project setup

---
+++ MessageView/MessageView.h
@@ -0,0 +1,19 @@
+//
+//  MessageView.h
+//  MessageView
+//
+//  Created by Joseph on 7/12/19.
+//  Copyright © 2019 Joseph. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MessageView.
+FOUNDATION_EXPORT double MessageViewVersionNumber;
+
+//! Project version string for MessageView.
+FOUNDATION_EXPORT const unsigned char MessageViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MessageView/PublicHeader.h>
+
+
---
+++ MessageViewDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MessageViewDemo
+//
+//  Created by Joseph on 7/12/19.
+//  Copyright © 2019 Joseph. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
